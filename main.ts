ddd3ec47d97846716596102145d3aa701c3104f9
Иван Баринов <i.barinov@itlabrealty.ru>
2023-12-24 14:55:51 +0300

    init

--- main.ts
+++ main.ts
@@ -1,81 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, DropdownComponent, MarkdownView, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CounterSettings {
+	folders: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CounterSettings = {
+	folders: []
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ObsidianCounter extends Plugin {
+	settings: CounterSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		this.registerEvent(
+			this.app.workspace.on('active-leaf-change', async (leaf) => {
+				const view = leaf?.view;
+				// if we are on the note tab
+				if (view && view instanceof MarkdownView) {
+					// @ts-ignore
+					const isDirectoryIgnored = !this.settings.folders.includes(view.file.parent.path)
+					if (isDirectoryIgnored) {
+						return
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					// get yaml frontmatter
+					const file: any = view.file;
+					const content = await readFile(file);
+					const lines = getYamlFrontMatter(content)
+					if (!lines) {
+						return
+					}
+					// get visited property
+					const visitedProperty = getVisitedProperty(lines)
+					if (!visitedProperty) {
+						return
+					}
+					let visitedValue: any = getPropertyValue(visitedProperty)
+					const visitedPropertyIndex = lines.indexOf(visitedProperty)
+					// update visited property
+					visitedValue = Number(visitedValue) + 1
+					lines[visitedPropertyIndex] = formProperty('visited', visitedValue)
+					const updatedFrontMatter = convertArrayToYamlFrontMatter(lines)
+					await this.app.vault.modify(file, uniteFrontMatterAndContent(updatedFrontMatter, content))
 				}
-			}
-		});
+			})
+		);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new CounterSettingsTab(this.app, this));
+		const readFile = async (file: any) => {
+			return await this.app.vault.read(file);
+		}
 	}
 
 	onunload() {
@@ -90,45 +65,113 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+const getYamlFrontMatter = (content: string) => {
+	const blocks = content.split('---').slice(1, -1);
+	if (blocks.length === 0) {
+		return null
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	return blocks[0].split('\n').filter(Boolean);
+}
+const getVisitedProperty = (lines: any[]) => {
+	const visitedProperty: any = lines.find((property) => property.includes('visited'))
+	return visitedProperty ?? null
+}
+const getPropertyValue = (property: string) => {
+	return property?.split(': ')[1]
+}
+const formProperty = (propertyName: string, value: any) => {
+	return `${propertyName}: ${value}`
+}
+const convertArrayToYamlFrontMatter = (lines: any[]) => {
+	return '---\n' + lines.join('\n') + '\n---'
+}
+const uniteFrontMatterAndContent = (frontMatter: string, content: string) => {
+	return `${frontMatter}${content.split('---')[2]}`
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class CounterSettingsTab extends PluginSettingTab {
+	plugin: ObsidianCounter;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ObsidianCounter) {
 		super(app, plugin);
 		this.plugin = plugin;
+	}
+
+	private _selectedFolders: string[] = [];
+	get selectedFolders(): string[] {
+		return this._selectedFolders;
+	}
+	set selectedFolders(value: string[]) {
+		this._selectedFolders = value;
 	}
 
 	display(): void {
 		const {containerEl} = this;
 
 		containerEl.empty();
+		let buttonContainer = document.createElement('div');
+
+		this.selectedFolders = this.plugin.settings.folders || [];
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Folders')
+			.setDesc('Select folders where the plugin should work')
+			.addTextArea(this.createTextArea());
+
+		const updateTextAreaField = this.createUpdateTextAreaHandler(containerEl);
+		const clearSelectionHandler = this.createClearSelectionHandler(updateTextAreaField);
+
+		new Setting(buttonContainer)
+			.addButton(button => button.setButtonText('Clear Selection').onClick(clearSelectionHandler))
+			.addDropdown(dropdown => {
+				dropdown.addOption('', 'Select Folders');
+				this.getUniqueFolders().forEach(folder => dropdown.addOption(folder, folder));
+				dropdown.onChange(this.createDropdownChangeHandler(dropdown, updateTextAreaField));
+			});
+
+		containerEl.appendChild(buttonContainer);
+	}
+	getUniqueFolders() {
+		return [...new Set(this.app.vault.getFiles().map(file => file.parent.path))];
+	}
+	private createTextArea() {
+		return (text: any) => {
+			text.inputEl.style.height = '200px';
+			text.setValue(this.selectedFolders.join(',\n'));
+			text.inputEl.setAttribute('readonly', 'true');
+		};
+	}
+	private createUpdateTextAreaHandler(containerEl: HTMLElement) {
+		return () => {
+			const textAreaField: HTMLTextAreaElement = containerEl.querySelector('textarea')!;
+			textAreaField.value = this.selectedFolders.join(',\n');
+		};
+	}
+	private createClearSelectionHandler(updateTextAreaField: Function) {
+		return async () => {
+			this.selectedFolders = [];
+			updateTextAreaField();
+			await this.updatePlugin()
+		};
+	}
+	private createDropdownChangeHandler(dropdown: DropdownComponent, updateTextAreaField: Function) {
+		return async (value: string) => {
+			if (!this.selectedFolders.includes(value)) {
+				this.selectedFolders.push(value);
+				this.selectedFolders = this.selectedFolders.filter(folder => folder);
+			} else {
+				const indexToRemove = this.selectedFolders.indexOf(value);
+				if (indexToRemove !== -1)
+					this.selectedFolders.splice(indexToRemove, 1);
+			}
+			dropdown.setValue('')
+			updateTextAreaField();
+			await this.updatePlugin()
+		};
+	}
+	private async updatePlugin() {
+		this.plugin.settings.folders = this.selectedFolders;
+		await this.plugin.saveSettings();
 	}
 }
+
