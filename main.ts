d4cc3b42f87c49019ca633ebcfc3f34f53fe6814
Иван Баринов <i.barinov@itlabrealty.ru>
2024-05-31 22:01:25 +0300

    Revert "extend settings for auto-create and apply to complete vault"
    This reverts commit 45e36c28f7034803e85215c9d197c71b4f31a930.

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
-import { App, DropdownComponent, MarkdownView, Plugin, PluginSettingTab, Setting, ToggleComponent, getFrontMatterInfo} from 'obsidian';
+import { App, DropdownComponent, MarkdownView, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 interface CounterSettings {
 	folders: string[];
-	autoCreate: boolean;
-	allFolders: boolean;
 }
 
 const DEFAULT_SETTINGS: CounterSettings = {
-	folders: [],
-	autoCreate: false,
-	allFolders: false
+	folders: []
 }
 
 export default class ObsidianCounter extends Plugin {
@@ -25,24 +21,20 @@ export default class ObsidianCounter extends Plugin {
 				if (view && view instanceof MarkdownView) {
 					// @ts-ignore
 					const isDirectoryIgnored = !this.settings.folders.includes(view.file.parent.path)
-					if (!this.settings.allFolders && isDirectoryIgnored) {
+					if (isDirectoryIgnored) {
 						return
 					}
 					// get yaml frontmatter
 					const file: any = view.file;
 					const content = await readFile(file);
-					let lines = getYamlFrontMatter(content)
-					let visitedProperty;
-					if (lines) {
-						visitedProperty = getVisitedProperty(lines)
-					} 
-					// create property if set to autocreate
-					if (!visitedProperty && this.settings.autoCreate) {
-						if (!lines) {
-								lines = []
-						}
-						visitedProperty = 'visited: 0'
-						lines.push(visitedProperty)
+					const lines = getYamlFrontMatter(content)
+					if (!lines) {
+						return
+					}
+					// get visited property
+					const visitedProperty = getVisitedProperty(lines)
+					if (!visitedProperty) {
+						return
 					}
 					let visitedValue: any = getPropertyValue(visitedProperty)
 					const visitedPropertyIndex = lines.indexOf(visitedProperty)
@@ -50,8 +42,7 @@ export default class ObsidianCounter extends Plugin {
 					visitedValue = Number(visitedValue) + 1
 					lines[visitedPropertyIndex] = formProperty('visited', visitedValue)
 					const updatedFrontMatter = convertArrayToYamlFrontMatter(lines)
-					const newContent = uniteFrontMatterAndContent(updatedFrontMatter, content)
-					await this.app.vault.modify(file, newContent)
+					await this.app.vault.modify(file, uniteFrontMatterAndContent(updatedFrontMatter, content))
 				}
 			})
 		);
@@ -75,7 +66,11 @@ export default class ObsidianCounter extends Plugin {
 	}
 }
 const getYamlFrontMatter = (content: string) => {
-	return getFrontMatterInfo(content).frontmatter.split('\n').filter(Boolean);
+	const blocks = content.split('---').slice(1, -1);
+	if (blocks.length === 0) {
+		return null
+	}
+	return blocks[0].split('\n').filter(Boolean);
 }
 const getVisitedProperty = (lines: any[]) => {
 	const visitedProperty: any = lines.find((property) => property.includes('visited'))
@@ -88,17 +83,10 @@ const formProperty = (propertyName: string, value: any) => {
 	return `${propertyName}: ${value}`
 }
 const convertArrayToYamlFrontMatter = (lines: any[]) => {
-	return lines.join('\n') + '\n'
+	return '---\n' + lines.join('\n') + '\n---'
 }
 const uniteFrontMatterAndContent = (frontMatter: string, content: string) => {
-	const frontMatterInfo = getFrontMatterInfo(content)
-	if (frontMatterInfo.exists) {
-		const contentBefore = content.substring(0, frontMatterInfo.from)
-		const contentAfter = content.substring(frontMatterInfo.to)
-		return String.prototype.concat(contentBefore, frontMatter, contentAfter)
-	} else {
-		return String.prototype.concat('---\n', frontMatter,'---\n', content)
-	}
+	return `${frontMatter}${content.split('---')[2]}`
 }
 
 class CounterSettingsTab extends PluginSettingTab {
@@ -125,7 +113,7 @@ class CounterSettingsTab extends PluginSettingTab {
 
 		this.selectedFolders = this.plugin.settings.folders || [];
 
-		const selectedFoldersSetting = new Setting(containerEl)
+		new Setting(containerEl)
 			.setName('Folders')
 			.setDesc('Select folders where the plugin should work')
 			.addTextArea(this.createTextArea());
@@ -133,7 +121,7 @@ class CounterSettingsTab extends PluginSettingTab {
 		const updateTextAreaField = this.createUpdateTextAreaHandler(containerEl);
 		const clearSelectionHandler = this.createClearSelectionHandler(updateTextAreaField);
 
-		const selectedFoldersButtons = new Setting(buttonContainer)
+		new Setting(buttonContainer)
 			.addButton(button => button.setButtonText('Clear Selection').onClick(clearSelectionHandler))
 			.addDropdown(dropdown => {
 				dropdown.addOption('', 'Select Folders');
@@ -142,31 +130,6 @@ class CounterSettingsTab extends PluginSettingTab {
 			});
 
 		containerEl.appendChild(buttonContainer);
-
-		new Setting(containerEl)
-			.setName('Use for all notes vault')
-			.setDesc('Toggles whether the plugin will be applied throughout every note in your vault')
-			.addToggle((toggle: ToggleComponent) => {
-				toggle.setValue(this.plugin.settings.allFolders)
-				toggle.onChange(async (value) => {
-					this.plugin.settings.allFolders = value
-					selectedFoldersButtons.setDisabled(value)
-					selectedFoldersSetting.setDisabled(value)
-					await this.plugin.saveSettings()
-				})
-			})
-
-		new Setting(containerEl)
-			.setName('Auto-create property')
-			.setDesc('Toggles whether the visited property is created automatically if not already existing')
-			.addToggle((toggle: ToggleComponent) => {
-				toggle.setValue(this.plugin.settings.autoCreate)
-				toggle.onChange(async (value) => {
-					this.plugin.settings.autoCreate = value
-					await this.plugin.saveSettings()
-					console.log("this", this)
-				})
-			})
 	}
 	getUniqueFolders() {
 		return [...new Set(this.app.vault.getFiles().map(file => file.parent.path))];
